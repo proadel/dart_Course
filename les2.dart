80e0ff15a5dfc7c4f04dd8ac86bccecd35ef1270
Adel Dawood <111702322+proadel@users.noreply.github.com>
2022-10-31 19:31:14 -0400

    les2 - var - int ...
    int

---
+++ les2.dart
@@ -0,0 +1,12 @@
+void main () {
+  print ('=-=-=-=-=-=-=-=-=-=-=');
+  print ('WELCOME to DART WORLD');
+  print ('=-=-=-=-=-=-=-=-=-=-=');
+  print ('les2 - VAR');
+  int a, b, c, d;
+  a = 5;
+  b = 5;
+  c = 5;
+  d = (a+b+c);
+  print ('total = ($d)');
+}
