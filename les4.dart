b19ffa5c443b31666c36a26e3f3fc56bd3d0efd5
Adel Dawood <111702322+proadel@users.noreply.github.com>
2022-11-02 17:23:33 -0400

    les4 ..

---
+++ les4.dart
@@ -0,0 +1,16 @@
+void main () {
+  print ("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-");
+  print ("+-+-+- Patint History Case e-sheet taking -+-+-+");
+  print ("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-");
+
+  String pt_Name = "sheri welson";
+  String pt_Gender = "female";
+  String pt_pHistory = "IHD";
+  int pt_Age = 43;
+  bool isFemale = true;
+  double pt_temp_cDegree = 38.1;
+  String pt_mComplain = "fever";
+
+  print ("$pt_Gender $pt_Age years old, named by $pt_Name come to my clinic today morning at 9:00am with $pt_mComplain around $pt_temp_cDegree ... etc");
+  
+}
