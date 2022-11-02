5f7ea3e340ff717abc3134a2fb327ad2d4369cf7
Adel Dawood <111702322+proadel@users.noreply.github.com>
2022-11-02 16:24:35 -0400

    double | String | {($var) ...}
    double | String | {($var) ...}

---
+++ les3.dart
@@ -0,0 +1,15 @@
+void main () {
+   print ("les2 - var | double - String");
+ double f1 = 3.14;
+ int f2 = 10;
+ double total_f = (f1 + f2);
+ String firstName = ('adel');
+ String lastName = ('mohammed');
+   print ("$firstName $lastName");
+   print ("Total = ($total_f)");
+
+ String name = "adel dawood";
+ String age = "43";
+    print ("$name is Student");
+    print ("ahmed is $age years old");
+}
