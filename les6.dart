6b0199956a4c0914542510584d1f1eeb8b739f95
Adel Dawood <111702322+proadel@users.noreply.github.com>
2022-11-03 16:27:26 -0400

    les6 / myFirst (+) dart_calcSys

---
+++ les6.dart
@@ -0,0 +1,30 @@
+import "dart:io";
+void main () {
+  print ("");
+  print ("     #########################");
+  print ("      myFirst (+) dart_calcSys");
+  print ("     #########################");
+  print ("");
+  print ("Please...............");
+  print ("Enter first_Number = ");
+  String? x=stdin.readLineSync(); //nullSafety ? | ~ | ! 
+  //print (x)
+
+  print ("++++++++++++++++++++++");
+  print ("Enter second_Number = ");
+  String? y=stdin.readLineSync(); //nullSafety ? | ~ | ! 
+  print ("====================================");
+  print ("Total = ");
+  //print (y);
+  print (double.parse(x!)+double.parse(y!));
+  print ("+++++++++++++++++++++++++++++++++++++");
+  print ("  Thanks to Use & Share my Projects");
+  print ("   by Adel MD |+Cisco CCNA_CCNP_IoE");
+  print ("   Full Stack Web & App.Developer");
+  print ("+++++++++++++++++++++++++++++++++++++");
+  print ("   be free to contact via whatsapp");
+  print ("   +967733477848 |!|!| Umbrella Corp.");
+  print ("_____________________________________");
+  print ("   Good Luck For myFamily++Friends");
+  print ("_____________________________________");
+  }
