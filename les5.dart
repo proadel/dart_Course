c3eb805b357e035c3e79534f797c22de134ae34c
Adel Dawood <111702322+proadel@users.noreply.github.com>
2022-11-03 16:25:04 -0400

    les5
    var.methode

---
+++ les5.dart
@@ -0,0 +1,12 @@
+void main () {
+  String name = 'Adel Mohammed Dawood';
+  int age = 43;
+  double x = 3.14;
+  bool 
+  print (name.isEmpty);
+  print (name.length);
+  print (age.bitLength);
+  print (name.bitLength);
+  print (x.bitLength);
+  print (name.indexOf('0'));
+}
