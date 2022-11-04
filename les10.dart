1683ac19365a95dd73971deeeb90368b5c0f2e6d
Adel Dawood <111702322+proadel@users.noreply.github.com>
2022-11-04 18:43:48 -0400

    les10

---
+++ les10.dart
@@ -0,0 +1,13 @@
+import "dart:io";
+void main () {
+  //--> infinity loop be care... 
+  //int x=0;
+  //while (x<5);
+  //print (x);
+  //the x++; resolve the problem of that loop...
+  // not working !!! tShoot....
+int x=0;
+while (x<5);
+print (x);
+x++;
+}
