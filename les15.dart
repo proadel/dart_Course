cb80fd35fee97a5ac0daba69117e9cf708298dce
Adel Dawood <111702322+proadel@users.noreply.github.com>
2022-11-06 07:25:29 -0500

    les15 -- > Function void .

---
+++ les15.dart
@@ -0,0 +1,35 @@
+void main (){
+// you can call your functions  as you wish without problems ,,,
+printHello();
+//printHello(); --> 
+welcomeBack();
+wcB2();
+soundRec1();
+goodBay1();
+
+}
+// how to make function | reCall function .. !!
+// "void" is the return type of the function --> !! :) so 
+// remember main in the start line of dart its function... thats it.
+//void main (){ }   --> main = Function. 
+void printHello () {
+  print ("=========================");
+  print ("  Hello Dear Customers");
+  print ("=========================");
+}
+
+void welcomeBack () {
+  print ("Welcome-back dear 'vesitor', How can i help you Today!! :)");
+}
+
+void wcB2 () {
+  print ("Please prepare Your info. Then Enter the code via SMS.. please after Alarm >>>");
+}
+
+void soundRec1 () {
+  print (" Sound Track ....MP4 etc.. Hello Dear ....");
+}
+
+void goodBay1 () {
+  print (" GOod Bay ..See Yaaaa Next Time .. Good Luck !!..");
+}
