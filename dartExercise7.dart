d935eb4a9bafa51f50abb5794d9ecc7ab523350a
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-11-26 09:48:48 -0800

    Conditional codes 2
    or = ||  
    and = &&
    --------------------
    if ( ..... || ..... ){ } 
    else{ }
    ================
    if (.... && ......){}
    else { }

---
+++ dartExercise7.dart
@@ -0,0 +1,14 @@
+// Conditional codes 2
+// if - else if -- else with :
+// ||=or , &&=and 
+void main(){
+var userName = 'DOdE';
+var passWord = 123456;
+//if (userName == 'DOdE' || passWord == 1234567){
+if (userName == 'DOdE' && passWord == 1234567){
+  print ('Welcome');
+}else{
+  print ('Wrong : Try Again..');
+}
+
+}
