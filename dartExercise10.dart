01a14cbbadec18c9ced60fd0f2c1ef353959846f
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-11-26 13:35:20 -0800

    Loop - While
    while ( var condition values ) {  codes;  }

---
+++ dartExercise10.dart
@@ -0,0 +1,17 @@
+// Loop - While
+//------------------
+void main (){
+var i = 0;
+while (i < 5){
+  //print ("numbre2: ${i}");
+  print ("numbre: $i");
+  i++;
+}
+
+//--> first -->ask i = 0 = yes <5 then , i < 5 = yes, then +1 & print
+//then ask1 new value of i now = 1 & is it!? < 5 = yes, then print 
+// -> i = 2 , ask2 <5 = yes the print .... until the total value of
+// i = 5 and thats what is not GOOD for the while conditioning 
+// code we made above == i<5 = NO. Then the loop stop. <5 = less
+// than 5 times. :)
+}
