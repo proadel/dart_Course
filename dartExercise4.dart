4fc1d89ba9ef7ee43f41b84670ee0c22d53774c7
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-11-26 07:03:52 -0800

    Logical operators
    Logical operators
    == | < > | && and | || or | != | ....., etc.

---
+++ dartExercise4.dart
@@ -0,0 +1,15 @@
+// Logical operators :| && and | "||" = or | < > | == | != | ...
+//--------------------------
+void main (){
+var num1=10;
+var num2=5;
+var num3=10;
+var y=100;
+//print (num1 == num2); // print=false
+//print (num1 != num2); // print=true
+//print (num2 > y); // print=false
+var ask;
+//ask = (num2 > num1 );
+ask = (num3 == num1); // print=true
+print (ask); // print=false !=
+}
