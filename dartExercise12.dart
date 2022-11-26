68ba30bbd171564bcab3e4dc1929fe2b9e5516c1
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-11-26 15:20:36 -0800

    index - [List] in Dart

---
+++ dartExercise12.dart
@@ -0,0 +1,16 @@
+// index - [List] in Dart
+// _List of Multi. data [String, int, boolean, double];
+void main(){
+var lanG = ['ruby','perl','python','go']; // String [_List]
+//var lanG = ['ruby[0]','perl[1]','python[2]','go[3]']; // String [_List]
+var numBers = [1,2,3,4,5]; // int [_List]
+//var numBers = [1[0],2[1],3[2],4[3],5[4]]; // int [_List]
+var daTa = ['Dode M.', 43, true, 3.14]; // multi objects [_List]
+//var daTa = ['Dode M.[0]', 43[1], true[2], 3.14[3]]; // multi objects [_List]
+ print (lanG [1]); // output: perl
+ print (numBers[3]);
+ print (daTa[1]);
+ //print (lanG);
+ //print (numBers);
+ //print (daTa);
+}
