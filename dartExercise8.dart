92191f8a5bef667c5198c19ef3b34a849735b5cf
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-11-26 10:26:52 -0800

    switch - case break - default
    switch ( VAR ) {
     case  value : {
      codes... ;
      break;
    }
    ......
    
    default {
      codes ... ;
    }
    }

---
+++ dartExercise8.dart
@@ -0,0 +1,21 @@
+// switch-case statements
+// switch (var){ case ... {codes... + break; } -->
+// default: {codes} 
+void main(){
+var number = 20;
+ switch (number) {
+  case 201: 
+  {
+    print ('true');
+    break; // to STOP codes if first case = condition or --> default
+  }
+  case 20:{
+    print ('true 2');
+    break;
+  }
+  default: 
+  {
+      print ('false');
+    }
+ }
+}
