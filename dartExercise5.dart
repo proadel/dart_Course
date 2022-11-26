e03a4e1d6df740c2601aad6ddc9fc248cb4201da
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-11-26 08:49:20 -0800

    Allocation factors
    Allocation factors

---
+++ dartExercise5.dart
@@ -0,0 +1,12 @@
+// Allocation factors 
+void main(){
+//var number =5;  
+double  number = 5;
+number += 5; //number = 5 +5 = 10
+number -= 2; //number = 10 -2 = 8
+number *= 2; //number = 8 *2 =16
+number /= 4; //print= Error: A value of type 'double' can't be
+             // /= 4; assigned to a variable of type 'int'.
+//double /=4;
+print (number);
+}
