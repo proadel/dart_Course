a18b96dd1478c7f668b8260e80b2dee299d809e3
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-11-26 09:25:47 -0800

    Conditional codes 1
    if( ) { } -- else if ( ) { } ---- else {  }

---
+++ dartExercise6.dart
@@ -0,0 +1,19 @@
+// Conditional codes: if | else if | else 
+// basic structure of conditional codes
+// if (){} else if (){} else{}
+// if (condition1){codes} --> not true then -->
+// else if (condtion2){codes} , else if , else if
+// --> not true --> else{codes}
+void main (){
+  var n = 11 ;
+  //print (n);
+  // becareful when coding your coditions.
+  //if(n !=10){ 
+  if(n == 10){ 
+    print ('thats True');
+  }else if (n == 11){
+    print ('Excelent');
+  }else{
+    print ('Wrong Try oncemore');
+  }
+}
