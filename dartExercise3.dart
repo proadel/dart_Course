ef51c30152884f096ccd78b2ce22c3127639a3cc
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-11-26 06:05:08 -0800

    mathematical operators
    mathematical operators

---
+++ dartExercise3.dart
@@ -0,0 +1,11 @@
+//mathematical operatorse: + | - | / | * |
+void main (){
+  //print (1 + 1); 
+  var number1 = 10;
+  var number2 = 20;
+  var math = number2 * number1 + 2; // 
+  //print (number1 / number2);
+  //print (number2 + number1);
+  //print (number1 * number2 / (-2));
+  print (math);
+}
