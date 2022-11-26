13c1b58af85de2c084f46acc222b8e5d9c9e5815
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-11-26 14:21:53 -0800

    Loop - do while
    do { codes;
        ..................  
    } while ( conditions ) ;

---
+++ dartExercise11.dart
@@ -0,0 +1,12 @@
+// Loop - do while
+void main (){
+  var x = 0;
+  do {
+    print ('ANSWER is : $x');
+    x++;
+  } while (x < 10);
+}
+// same-as other l_oops.
+// do { 
+//  codes
+//} while (conditions and methods);
