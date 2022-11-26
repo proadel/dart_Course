d6a12a104efe41cc18e098d47d48b56cfa9e95e3
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-11-26 11:01:16 -0800

    Loop - for
    for ( var ; condition ; ++ or -- ) { }

---
+++ dartExercise9.dart
@@ -0,0 +1,17 @@
+// Loop - for
+// for (VAR ; condition ; var-- or var++) {codes }
+// note : -- = var-1 =
+// note : ++ = var+1 = 
+void main () {
+ /*print ('1');
+ print ('2');
+ print ('3');
+ print ('...');
+ print ('1000');*/
+ //for (int i = 0; i = 5 ; i++){ //output Error: A value of 
+ //type 'int' can't be assigned to a variable of type 'bool'
+   //for (int i = 0; i <= 5 ; i++){ //output: 0 --> 5 .
+   for (int i = 0; i < 1000 ; i++){ //output: 0 -->99 .
+   print (i);
+ }
+}
