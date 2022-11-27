be84b32e85322cfa312ea9ae4edf0c8a3e40cb42
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-11-27 10:34:25 +0300

    Date entry in Dart
    import lib dart:io
    using of stdin.readLineSync ( ) to activate data entry

---
+++ dartExercise15.dart
@@ -0,0 +1,16 @@
+// Date entry in Dart
+//
+import 'dart:io';
+void main (){
+ print ('Enter Your Name:');
+ var name = stdin.readLineSync(); // to use stdin import lib dart:io
+ print ('your name is: $name');
+}
+
+// to run this types of code you should use Terminal
+// C:\yourpaths..\\ > dart filename.dart --> Enter
+// because the extension code.run can not display
+// the outputs in cases like this 
+// DO NOT Forget using import lip dart:io 
+// to activation of codes like stdin
+// Thank you.
