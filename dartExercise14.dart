aa3336c9b3723ef29290d1d13c76316cf330ae8c
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-11-27 10:17:37 +0300

    map in dart

---
+++ dartExercise14.dart
@@ -0,0 +1,16 @@
+// map in dart
+// 'Map<int, Object>'
+// var var.name = new Map(); --> for creating map
+// var.name ['key'] = [value];
+void main(){
+var m1 = new Map();
+m1 ['name'] = ['Adel'];
+m1 ['age'] = 43;
+print (m1);
+
+//var m2 = {1 (key number) : [value]'KEY1_&_value1', 2: 'KEY1_&_value1'};
+//var m2 = {1: 'AHMED', 2: 43}; //--> to creat new map "another way"
+var m2 = {1: 'adel', 2: 43, 3: 'male', 4: 'smoker'};
+print (m2);
+
+}
