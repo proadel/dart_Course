39c819ddf19e308fd4bec62302c8813cf6778200
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-11-27 10:56:22 +0300

    Dart TShoot  |  Try and catch (ex)

---
+++ dartExercise16.dart
@@ -0,0 +1,16 @@
+// Dart TShoot | Try and catch(ex)
+// try finde more info about (ex)
+void main() {
+  try{
+    // HERE enter your codes and try-code try to operate
+    // the codes and if its not working well i mean codes
+    // then catch method it takes action to display and 
+    // find ERRORS to Tshoot Your codes 
+    var a = 5;
+    var b = 10;
+    //var b = hg44;
+    print (a+b);
+  } catch (ex){
+    print (ex);
+  }
+}
