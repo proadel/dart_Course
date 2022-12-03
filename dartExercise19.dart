ee8b0fbdf04ce0a758c0f3390a219282a079b45a
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-12-03 19:12:39 +0300

    function

---
+++ dartExercise19.dart
@@ -0,0 +1,20 @@
+void main(){
+welcome();
+myName('adel');
+math(10,11);
+}
+void welcome(){
+  print ('Welcome to learn Dart2022');
+  print ('Adel MD. | Umbrella Corp.');
+}
+void myName(var name){
+  print ('name');
+}
+void math(n1,n2){
+  print (n1 + n2);
+  if (n1 == n2){
+    print ('true');
+  }else{
+    print ('false');
+  }
+}
